5ec48caa31923880f2fb0c8ce4e574e2333b7c81
Yatex <foliver@gmail.com>
2020-10-24 17:15:07 -0300

    WAVE cambios

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -4,7 +4,7 @@
             <v-row align="center"  justify="space-between">
                 <v-card-text>
                     <v-card-subtitle>Score:</v-card-subtitle>
-                    <v-rating v-model="card.rating" readonly :length="length" color="yellow" background-color="grey lighten-1"></v-rating>
+                    <v-rating v-model="card.rating" readonly :length="length" color="yellow darken-4" background-color="grey darken-4"></v-rating>
                     <v-card-subtitle>Review:</v-card-subtitle>
                     <p class="ml-5">{{ card.review}}</p>
                 </v-card-text>
