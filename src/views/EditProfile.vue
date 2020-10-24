ffe011edd0083d53f32cd03fe3c2ba69cf694887
Yatex <foliver@gmail.com>
2020-10-24 16:56:22 -0300

    cambio en profile para WAVE

--- src/views/EditProfile.vue
+++ src/views/EditProfile.vue
@@ -23,15 +23,9 @@
 
             <v-text-field label="PhoneNumber" prepend-icon="mdi-phone" type="text" color="teal-accent-3"
                 v-model="phone"  :rules="PhoneRules" :readonly="rdonly"/>
-
-            <v-menu ref="menu" v-model="menu" :close-on-content-click="false" transition="scale-transition" offset-y min-width="290px" >
-                <!-- <v-menu-item> -->
-                  <template v-slot:activator="{ on, attrs }">
-                      <v-text-field v-model="date" label="Birthday date" prepend-icon="mdi-calendar" :readonly="rdonly" v-bind="attrs" v-on="on" ></v-text-field>
-                  </template>
-                  <v-date-picker ref="picker" v-model="date" max="2030-01-01" min="1950-01-01" @change="save" ></v-date-picker>
-                <!-- </v-menu-item> -->
-            </v-menu>
+                
+            <v-text-field v-model="date" label="Birthday date" prepend-icon="mdi-calendar" :readonly="rdonly" v-bind="attrs" v-on="on" ></v-text-field>
+              
             
             <v-select :items="genders" label="Gender" prepend-icon="mdi-gender-male-female" v-model="gender" item-value="value" item-text="text" :readonly="rdonly"></v-select>
           </v-form>
