657de1602905be8aa73906a952a34403377a7cfe
Yatex <foliver@gmail.com>
2020-10-23 21:31:01 -0300

    main con todas las cosas andando

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -6,17 +6,12 @@
       <v-app id="inspire">
         <v-container class="grey lighten-5">
           <v-row align="center" justify="space-around" >
-            <v-col sm="4">
-              <v-row no-gutters>
-                <HomeCard topLine='Crear rutina de ejercicios' bottomLine='Empezar una nueva rutina.'/>
-              </v-row>
+            <v-col cols="5">
+              <ImageTitleCard :card="{ title: 'My Favourites', img:'https://images.pexels.com/photos/256807/pexels-photo-256807.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', link:'/favourites' }"/>
             </v-col>
-            <v-col cols="5" md="4">
-              <ImageTitleCard :card="{ title: 'Profile', img:'https://images.pexels.com/photos/2294354/pexels-photo-2294354.jpeg?cs=srgb&dl=pexels-li-sun-2294354.jpg&fm=jpg', link:'/editprofile' }"/>
+            <v-col cols="5">
+              <ImageTitleCard :card="{ title: 'My Profile', img:'https://images.pexels.com/photos/2294354/pexels-photo-2294354.jpeg?cs=srgb&dl=pexels-li-sun-2294354.jpg&fm=jpg', link:'/editprofile' }"/>
             </v-col>
-            <v-row no-gutters>
-                <HomeCard topLine='Crear nuevos Ejercicios' bottomLine='Armar instructivos de un ejercicio.'/>
-              </v-row>
           </v-row>
           <v-row  justify="space-around">
             <v-col cols="5">
@@ -38,14 +33,14 @@
 
   import Navbar from '../components/Navbar';
   import Footer from '../components/Footer';
-  import HomeCard from '../components/HomeCard';
+  // import HomeCard from '../components/HomeCard';
   import ImageTitleCard from '../components/ImageTitleCard';
 
   export default {
     components: {
       Navbar,
       Footer,
-      HomeCard,
+      // HomeCard,
       ImageTitleCard
     },
   }
